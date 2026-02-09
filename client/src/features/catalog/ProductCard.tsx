a9439b97395f26b10d0c7fec8c95701f6b5b8286
BaraaOthman <baraaothman712@gmail.com>
2026-02-10 00:40:20 +0200

    End of section 4

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,50 +1,48 @@
-import React from "react";
-import type { Product } from "../../app/models/product";
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Typography,
-} from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
+import { Product } from "../../app/models/product"
+import { Link } from "react-router-dom"
 
 type Props = {
-  product: Product;
-};
-export default function ProductCard({ product }: Props) {
+    product: Product
+}
+
+export default function ProductCard({product}: Props) {
   return (
     <Card
-      elevation={3}
-      sx={{
-        width: 200,
-        borderRadius: 2,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
+        elevation={3}
+        sx={{
+            width: 280,
+            borderRadius: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        }}
     >
-      <CardMedia
-        sx={{ height: 240, backgroundSize: "cover" }}
-        image={product.pictureUrl}
-        title={product.name}
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          sx={{ textTransform: "uppercase" }}
-          variant="subtitle2"
+        <CardMedia 
+            sx={{height: 240, backgroundSize: 'cover'}}
+            image={product.pictureUrl}
+            title={product.name}
+        />
+        <CardContent>
+            <Typography 
+                gutterBottom
+                sx={{textTransform: 'uppercase'}} 
+                variant="subtitle2">
+                    {product.name}
+            </Typography>
+            <Typography
+                variant="h6"
+                sx={{color: 'secondary.main'}}
+            >
+                ${(product.price / 100).toFixed(2)}
+            </Typography>
+        </CardContent>
+        <CardActions
+            sx={{justifyContent: 'space-between'}}
         >
-          {product.name}
-        </Typography>
-        <Typography variant="h6" sx={{ color: "secondary.main" }}>
-          ${(product.price / 100).toFixed(2)}
-        </Typography>
-      </CardContent>
-      <CardActions sx={{ justifyContent: "space-between" }}>
-        <Button> Add to cart</Button>
-        <Button> View </Button>
-      </CardActions>
+            <Button>Add to cart</Button>
+            <Button component={Link} to={`/catalog/${product.id}`}>View</Button>
+        </CardActions>
     </Card>
-  );
+  )
 }
